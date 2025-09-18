962f7b9bb7c643ab42d8788f691750d1b9410f10
Ruijia Zhou <u6537308@anu.edu.au>
2025-09-18 16:13:26 +1000

    LeetCode 994 & 2327

--- src/PeopleAwareOfSecret.java
+++ src/PeopleAwareOfSecret.java
@@ -1,43 +1,36 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 // LeetCode 2327
 public class PeopleAwareOfSecret {
-    // fixed moduler
-    private static final int MOD = 1000000007;
-    // main function to find the num of people aware of secret
+    private static final int FIXED_MODULAR = 1000000007;
+
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        // a secret buffer recording num of people knowing secret of diff days
         int[] secretBuffer = new int[forget + 1];
         secretBuffer[1] = 1;
-        // simulate the secret sharing process from Day 2
         for (int i = 2; i <= n; i++) {
             shareSecret(secretBuffer, delay, forget);
         }
-        return countNumPeople(secretBuffer);
+        return countNumPeopleAwareOfSecretsOnFinalDay(secretBuffer);
     }
-    // simulate the secret sharing process
+
     private void shareSecret(int[] secretBuffer, int delay, int forget) {
         int dayBefore = 0;
-        // update num of secret-aware people of diff days
         for (int j = 1; j <= forget; j++) {
             int temp = secretBuffer[j];
             secretBuffer[j] = dayBefore;
             dayBefore = temp;
             if (j >= delay && j < forget) {
                 secretBuffer[1] += temp;
-                secretBuffer[1] %= MOD;
+                secretBuffer[1] %= FIXED_MODULAR;
             }
         }
     }
-    // count num of people aware of a secret on final day
-    private int countNumPeople(int[] secretBuffer) {
-        int numPeople = 0;
+
+    private int countNumPeopleAwareOfSecretsOnFinalDay(int[] secretBuffer) {
+        int numPeopleKnowingSecret  = 0;
         // sum up all people knowing the secret
         for (int numPerDay : secretBuffer) {
-            numPeople += numPerDay;
-            numPeople %= MOD;
+            numPeopleKnowingSecret  += numPerDay;
+            numPeopleKnowingSecret  %= FIXED_MODULAR;
         }
-        return numPeople;
+        return numPeopleKnowingSecret ;
     }
 }
--- src/RottenOranges.java
+++ src/RottenOranges.java
@@ -1,70 +1,60 @@
-
-
+// LeetCode 994
+import com.google.common.collect.ImmutableList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// LeetCode 994
 class RottenOranges {
-    // global field for storing the grids
     private int[][] grid;
     private int numRows;
     private int numCols;
-    // global field for BFS
-    private final Queue<Coordinate> rottenOranges = new LinkedList<>();
-    private final int[] dx = {0, 1, 0, -1};
-    private final int[] dy = {1, 0, -1, 0};
     private int numOranges = 0;
     private int numRottenOranges = 0;
+    private final Queue<Coordinate> rottenOranges = new LinkedList<>();
+    private static final int EMPTY_GRID = 0;
+    private static final int GOOD_ORANGE = 1;
+    private static final int ROTTEN_ORANGE = 2;
+    private static final ImmutableList<Integer> DELTA_X = ImmutableList.of(0, 1, 0, -1);
+    private static final ImmutableList<Integer> DELTA_Y = ImmutableList.of(1, 0, -1, 0);
+
     public int orangesRotting(int[][] grid) {
-        // store parameters
         this.grid = grid;
         this.numRows = grid.length;
         this.numCols = grid[0].length;
-        // traverse the grids to count num of oranges
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                if (grid[i][j] == 2) this.rottenOranges.add(new Coordinate(i, j));
-                if (grid[i][j] != 0) this.numOranges++;
+                if (grid[i][j] == ROTTEN_ORANGE) this.rottenOranges.add(new Coordinate(i, j));
+                if (grid[i][j] != EMPTY_GRID) this.numOranges++;
             }
         }
-        // BFS to iteratively simulate the infection process
         return BFS();
     }
+
     private int BFS() {
-        int numMinites = 0;
+        int numMinutes = 0;
         while (!this.rottenOranges.isEmpty()) {
-            numMinites++;
-            // iterate through rotten oranges in current minute
+            numMinutes++;
             int curRottenOranges = this.rottenOranges.size();
             for (int i = 0; i < curRottenOranges; i++) {
                 Coordinate curOrange = this.rottenOranges.poll();
                 this.numRottenOranges++;
-                int curX = curOrange.x;
-                int curY = curOrange.y;
-                // infect adjacent oranges if applicable
-                for (int j = 0; j < 4; j++) {
-                    infectAdjacent(curX + dx[j], curY + dy[j]);
+                int curX = curOrange.x();
+                int curY = curOrange.y();
+                for (int j = 0; j < DELTA_X.size(); j++) {
+                    infectAdjacent(curX + DELTA_X.get(j), curY + DELTA_Y.get(j));
                 }
             }
         }
-        // deduct one from num of minutes as the last round doesn't add any new rotten oranges
-        numMinites = Math.max(0, numMinites - 1);
-        return this.numRottenOranges == this.numOranges ? numMinites : -1;
+        numMinutes = Math.max(0, numMinutes - 1);
+        return this.numRottenOranges == this.numOranges ? numMinutes : -1;
     }
+
     private void infectAdjacent(int newX, int newY) {
-        // check for boundaries and existence of oranges
         if (newX < 0 || newX >= this.numRows ||
-                newY < 0 || newY >= numCols ||
-                grid[newX][newY] != 1) return;
-        this.grid[newX][newY] = 2;
+                newY < 0 || newY >= this.numCols ||
+                this.grid[newX][newY] != GOOD_ORANGE) return;
+        this.grid[newX][newY] = ROTTEN_ORANGE;
         this.rottenOranges.add(new Coordinate(newX, newY));
     }
-    private static class Coordinate{
-        int x;
-        int y;
-        Coordinate(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+
+    private record Coordinate(int x, int y){}
 }
