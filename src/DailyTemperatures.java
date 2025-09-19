abef3f301cd273213f386b4c27a48d6f94032105
Ruijia Zhou <u6537308@anu.edu.au>
2025-09-19 23:19:52 +1000

    LeetCode 739

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -3,18 +3,25 @@ import java.util.Deque;
 
 // LeetCode 739
 public class DailyTemperatures {
+    private int[] temperatures;
+    private int[] waitingDays;
+    private Deque<Integer> colderDays;
+
     public int[] dailyTemperatures(int[] temperatures) {
         int numDays = temperatures.length;
-        int[] waitingDays = new int[numDays];
-        Deque<Integer> colderDays = new ArrayDeque<>();
-        for (int currDay = 0; currDay < numDays; currDay++) {
-            int currentTemp = temperatures[currDay];
-            while (!colderDays.isEmpty() && temperatures[colderDays.peek()] < currentTemp) {
-                int prevDay = colderDays.pop();
-                waitingDays[prevDay] = currDay - prevDay;
-            }
-            colderDays.push(currDay);
-        }
+        this.temperatures = temperatures;
+        this.waitingDays = new int[numDays];
+        this.colderDays = new ArrayDeque<>();
+        for (int currDay = 0; currDay < numDays; currDay++) updateOldDays(currDay);
         return waitingDays;
+    }
+
+    private void updateOldDays(int currDay){
+        int currentTemp = this.temperatures[currDay];
+        while (!this.colderDays.isEmpty() && this.temperatures[this.colderDays.peek()] < currentTemp) {
+            int prevDay = this.colderDays.pop();
+            this.waitingDays[prevDay] = currDay - prevDay;
+        }
+        this.colderDays.push(currDay);
     }
 }
