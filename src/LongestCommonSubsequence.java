7fa5c27379c4d2f0f12785b81a7ee2ba248374f9
Ruijia Zhou <u6537308@anu.edu.au>
2025-09-19 22:32:38 +1000

    LeetCode 1143

---
+++ src/LongestCommonSubsequence.java
@@ -0,0 +1,18 @@
+// LeetCode 1143
+public class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1.length() < text2.length()) return longestCommonSubsequence(text2, text1);
+        int[] dp = new int[text2.length() + 1];
+        int pred = 0;
+        for (int i = 0; i <= text1.length(); i++) {
+            for (int j = 0; j <= text2.length(); j++) {
+                int temp = dp[j];
+                if (i == 0 || j == 0) dp[j] = 0;
+                else if (text1.charAt(i - 1) == text2.charAt(j - 1)) dp[j] = pred + 1;
+                else dp[j] = Math.max(dp[j], dp[j - 1]);
+                pred = temp;
+            }
+        }
+        return dp[text2.length()];
+    }
+}
